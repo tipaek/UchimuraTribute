f949d3a19c2be2582e445bbbc9a301d97b5039c2
485926315-su <143752294+485926315-su@users.noreply.github.com>
2023-12-15 11:16:39 -0500

    add files

---
+++ code/script.js
@@ -0,0 +1,35 @@
+function openPic1()
+{
+    document.getElementById("image1").style.display = "block";
+}
+function closePic1()
+{
+    document.getElementById("image1").style.display = "none";
+}
+
+function openPic2()
+{
+    document.getElementById("image2").style.display = "block";
+}
+function closePic2()
+{
+    document.getElementById("image2").style.display = "none";
+}
+
+function openPic3()
+{
+    document.getElementById("image3").style.display = "block";
+}
+function closePic3()
+{
+    document.getElementById("image3").style.display = "none";
+}
+
+function openPic4()
+{
+    document.getElementById("image4").style.display = "block";
+}
+function closePic4()
+{
+    document.getElementById("image4").style.display = "none";
+}
